98dae0196a973b4302cd7365b2bd8d86da1e82af
javi <fordrsmax@gmail.com>
2024-08-13 18:32:24 +0200

    change 3d model of car and add first colision checks

--- main.gd
+++ main.gd
@@ -1,10 +1,18 @@
 extends Node3D
 
-const horsepower = 10
+const horsepower = 20
 const maxSteerAngle = 0.5
 
 var cameras = []
 var current_camera_index = 0
+
+var thAxis = 0
+var stAxis = 0
+var brake = 0
+var rev = false
+
+var carBody3d = null
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 
@@ -16,33 +24,52 @@ func _ready():
 	if cameras.size() > 0:
 		cameras[current_camera_index].current = true
 
+	carBody3d = $Coche.get_node("VehicleBody3D")
+	
+	$CanvasLayer/Label.text = "loading..."
+	
+	
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass
+
+func _physics_process(delta):
+
+	if carBody3d != null:
+
+		var gas = 0
+		if rev && thAxis > 0:
+			gas = -thAxis
+		else :
+			gas = thAxis
+			
+		carBody3d.set_engine_force(gas * horsepower)
+		carBody3d.set_steering(stAxis * maxSteerAngle)
+		carBody3d.set_brake(brake)
+		
+		$CanvasLayer/Label.text = "R " + str(rev) + " brake " + str(brake) + " th " + str(gas) + " st " + str(stAxis)
+	pass
+
+# called on input change
+func _input(event):
+
+	# actualizamos los valores de los ejes
+	stAxis = Input.get_axis("std", "sti")
+	
+	#---------actions proces-----------
+	if Input.is_action_just_pressed("brk"):
+		brake = 1
+	elif Input.is_action_just_released("brk"):
+		brake = 0
+
 	if Input.is_action_just_pressed("toggle_camera"):
 		change_camera()
+		
+	thAxis = Input.get_action_strength("th")
 
-func _input(event):
-	
-   # Mouse in viewport coordinates.
-	if event is InputEventMouseButton:
-		#print("Mouse Click/Unclick at: ", event.position)
-		pass
-	elif event is InputEventMouseMotion:
-		#print("Mouse Motion at: ", event.relative)
-		var f1 = Vector3(event.relative.x, -event.relative.y, 0)
-	
-	#---------procesado para ordenador------------
-	
-	#eje accelerador
-	var thAxis = Input.get_axis("brk", "th")
-	$Coche.get_node("VehicleBody3D").set_engine_force(thAxis * horsepower)
-	#var brk = Input.get_action_strength("brk")
-			
-	#comporbacion eje de direccion
-	var stAxis = Input.get_axis("std", "sti")
-	$Coche.get_node("VehicleBody3D").set_steering(stAxis * maxSteerAngle)
-
-	#print("eje de direccion ",stAxis ," eje de acelerador ",thAxis," freno ",brk)
+	if Input.is_action_pressed("rev"):
+		rev = !rev
 	
 #aux functions
 func change_camera():
