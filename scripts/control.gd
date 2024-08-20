35decd08d03a872d3de90428b8d6997853060a87
javi <fordrsmax@gmail.com>
2024-08-20 17:58:58 +0200

    add gear indicator in ui

--- scripts/control.gd
+++ scripts/control.gd
@@ -1,11 +1,28 @@
 extends Control
 
+var uiRev = false
+
+var img
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	img = $TextureRect
 	$ScoreLabel.text = "0"
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$ScoreLabel.text = str(ScoreManager.getScore())
+	
+	if(uiRev):
+		img.texture = load("res://assetts/images/rear.svg")		
+	else:
+		img.texture = load("res://assetts/images/fwd.svg")
+	
 	pass
+	
+func _input(event):
+	
+	if Input.is_action_pressed("rev"):
+		uiRev = !uiRev
+	
