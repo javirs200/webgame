23f6a99ad1460a5be215b61c356e1de43e2ed627
javi <fordrsmax@gmail.com>
2024-10-10 13:12:15 +0200

    add button suport for gamepads

--- scripts/CarController.gd
+++ scripts/CarController.gd
@@ -1,35 +1,41 @@
 extends Node
 
-const horsepower = 50
-const maxSteerAngle = 0.6
+const horsepower:float = 50
+const maxSteerAngle:float = 0.6
 
-var thAxis = 0
-var stAxis = 0
-var brake = 0
+var thAxis:float = 0
+var stAxis:float = 0
+var brake:float = 0
 
-var carBody3d
+var carBody3d: VehicleBody3D
 
-var cubesDetector = true
+var cubesDetector:bool = true
 
-var score = 0
+var score:int = 0
+
+var startingPosition: Vector3
+
 
 func _ready():
+	
+	# record Starting position
+	startingPosition = $VehicleBody3D.global_position
 	
 	carBody3d = $VehicleBody3D
 	
 	if carBody3d != null:
 		
 		carBody3d.contact_monitor = true
-		carBody3d.max_contacts_reported = 2	
+		carBody3d.max_contacts_reported = 2
 
-		carBody3d.connect("body_entered",self._on_vehicle_body_entered)
+		carBody3d.connect("body_entered", self._on_vehicle_body_entered)
 
 func _physics_process(delta):
 	if carBody3d != null:
 		var gas = 0
 		if Utils.getGear() && thAxis > 0:
 			gas = -thAxis
-		else :
+		else:
 			gas = thAxis
 			
 		carBody3d.set_engine_force(gas * horsepower)
@@ -37,44 +43,53 @@ func _physics_process(delta):
 		carBody3d.set_brake(brake)
 pass
 
-# called on input change
+# called on input change on phisical device not touch screen
 func _input(event):
+	
 	# actualizamos los valores de los ejes
-	stAxis = Input.get_axis("ui_right", "ui_left")
+	stAxis = Input.get_axis("ui_right", "ui_left") # left stick left and right on gamepad or left and right arrows on keyboard
 
 	#---------actions proces-----------
-	if Input.is_action_just_pressed("ui_down"):
+	if Input.is_action_just_pressed("ui_down"): # Down on keyboard or left stick down on gamepad
 		brake = 1
 	elif Input.is_action_just_released("ui_down"):
 		brake = 0
 		
-	thAxis = Input.get_action_strength("ui_up")
+	thAxis = Input.get_action_strength("ui_up") # Up on keyboard or left stick up on gamepad
 
-	if Input.is_action_pressed("rev"):
-		print("reverse")
+	if Input.is_action_pressed("rev"): # R on keyboard
+		print("reverse keyboard")
 		Utils.swapGear()
+	elif Input.is_action_pressed("ui_select"): # Y on Xbox controller or Triangle on PS4 controller or space on keyboard
+		print("reverse gamepad")
+		Utils.swapGear()
+
+	if Input.is_action_pressed("ui_accept"): # A on Xbox controller or Cross on PS4 controller or Enter on keyboard
+		print("starting pos " + str(startingPosition) + " actual pos " + str(carBody3d.position))
+		print("reseting request")
+		print("starting pos " + str(startingPosition) + " actual pos " + str(carBody3d.position))
 		
-	if Input.is_action_pressed("ui_cancel"):
+	if Input.is_action_pressed("ui_cancel"): # B on Xbox controller or Circle on PS4 controller or ESC on keyboard
 		get_tree().quit()
 	
 func _on_vehicle_body_entered(body: PhysicsBody3D):
 	#print("VehicleBody3D collided with: " + body.name)
 	
 	#check if collision body is a cube and increase score , <- Car Cube Cetector
-	if(body.name == "cubo1" && cubesDetector):
+	if (body.name == "cubo1" && cubesDetector):
 		ScoreManager.increaseScore(1)
 		moveAndRestart(body.get_parent_node_3d())
 
-func moveAndRestart(cubeNode:Node3D):
+func moveAndRestart(cubeNode: Node3D):
 	# disable car cube detector 
-	cubesDetector = false 
+	cubesDetector = false
 	
 	#stores child collider and removes them
 	var child = cubeNode.get_child(0)
-	child.position = Vector3(0,-2,0)
+	child.position = Vector3(0, -2, 0)
 	
 	#enlarge the cube and wait 1 second
-	cubeNode.scale = Vector3(2,2,2)
+	cubeNode.scale = Vector3(2, 2, 2)
 	await get_tree().create_timer(1).timeout
 	
 	#hides the cube and search for another position and wait 0,5 seconds
@@ -86,9 +101,10 @@ func moveAndRestart(cubeNode:Node3D):
 	cubesDetector = true
 	cubeNode.scale = Vector3(1, 1, 1)
 	cubeNode.visible = true
-	child.position = Vector3(0,0,0)
+	child.position = Vector3(0, 0, 0)
+	
 		
-func generateRandomPosition(Areax : int, Areaz : int) -> Vector3:
+func generateRandomPosition(Areax: int, Areaz: int) -> Vector3:
 	var randomX = randi_range(-Areax, Areax)
 	var randomZ = randi_range(-Areaz, Areaz)
 	return Vector3(randomX, 0.645, randomZ)
--- scripts/sceneNavigator.gd
+++ scripts/sceneNavigator.gd
@@ -1,11 +1,19 @@
 extends Control
 
+var navigate = false
+
 func _input(event):
 	if event is InputEventScreenTouch:
 		if event.pressed:
 			if _is_point_inside(event.position):
-				get_tree().change_scene_to_file("res://scenes/main.tscn")
-				
+				navigate = true
+	elif Input.is_action_pressed("ui_select"):
+		navigate = true
+		
+	if navigate:
+		get_tree().change_scene_to_file("res://scenes/main.tscn")
+		
+	
 func _is_point_inside(point: Vector2) -> bool:
 	var x: bool = point.x >= global_position.x and point.x <= global_position.x + (size.x * get_global_transform_with_canvas().get_scale().x)
 	var y: bool = point.y >= global_position.y and point.y <= global_position.y + (size.y * get_global_transform_with_canvas().get_scale().y)
